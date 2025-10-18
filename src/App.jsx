8e90dee2166131301b446d93885603fae5e8399b
Ubuntu <azureuser@VM1.bpheyu1op2pu1hq3thlcrqm2bd.bx.internal.cloudapp.net>
2025-10-18 15:01:24 +0000

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 Hello from My React App!</h1>
       <p>This app is running successfully!</p>
+      <p>This is second attempt of CI CD pipeline after inplemeting deploy.yml in Github Actions</p>	  
     </div>
   );
 }
